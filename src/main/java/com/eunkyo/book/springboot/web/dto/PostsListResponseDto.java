078c14897c94729b7051a1f97ebfe8c534ad154d
oeunkyoung <ou1112@naver.com>
2020-04-12 15:18:11 +0900

    chapter 5 ing

---
+++ src/main/java/com/eunkyo/book/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.eunkyo.book.springboot.web.dto;
+
+import com.eunkyo.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
